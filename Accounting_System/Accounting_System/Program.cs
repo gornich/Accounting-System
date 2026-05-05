06a8af330bdfa01f6474ae74cc867655bad60e92
gornich <gera.hlistov@yandex.ru>
2026-05-05 20:24:41 +0400

    First Commit

---
+++ Accounting_System/Accounting_System/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            add_expense(5000);
+        }
+
+        static public void add_expense(int sum)
+        {
+            Console.WriteLine($"Добавлен расход: {sum}руб");
+        }
+    }
+}
